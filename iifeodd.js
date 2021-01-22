425373d5c04e58ba09914709e5b60dc1125cf9a5
Abhiram-932 <77227232+Abhiram-932@users.noreply.github.com>
2021-01-22 21:37:41 +0530

    Add files via upload

---
+++ iifeodd.js
@@ -0,0 +1,11 @@
+(function(a)
+{
+    for(i=0;i<a.length;i++)
+    {
+        if(a[i]%2!=0)
+        {
+          console.log(a[i]);  
+        }
+    }
+})
+([1,2,7,4,5])
---
+++ iiferotate.js
@@ -0,0 +1,10 @@
+(function(arr,k)
+{
+    var i=0
+    while(i<k)
+    {
+        arr.unshift(arr.pop())
+        i++
+    }
+    console.log(arr);
+})([1,2,3,4,5],3)
